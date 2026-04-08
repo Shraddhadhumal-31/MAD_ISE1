b3f5a809858bea2e14fdc271fe01b4c51617a774
Shraddhadhumal-31 <shraddhadhumal31@gmail.com>
2026-04-08 23:03:22 +0530

    Add files via upload

---
+++ Assignment No.4/fragment/MyFragment.java
@@ -0,0 +1,22 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.fragment.app.Fragment;
+public class MyFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_layout, container, false);
+
+        TextView tv = view.findViewById(R.id.tvFragment);
+        tv.setText("This is Fragment");
+
+        return view;
+    }
+}
